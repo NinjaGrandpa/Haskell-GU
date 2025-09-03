9842a56454d9c33010b94f73abfda4eb1496dbf5
Max Jenslöv <max@jenslov.se>
2025-09-03 11:54:46 +0200

    Created lec2a and lec3a

---
+++ lec2a.hs
@@ -0,0 +1,12 @@
+-- :t command show the type of any expression
+-- The "::"-symbol indicates a type signature, "is of type"
+
+-- Prelude> :type True
+-- True :: Bool
+-- Prelude> :type False
+-- False :: Bool
+-- Prelude> :t (3 < 5)
+-- (3 < 5) :: Bool
+
+-- chr :: Int -> Char, decodes int to char
+-- ord :: Char -> Int, encodes a char to a numeric code
---
+++ lec3a.hs
@@ -0,0 +1,44 @@
+-- Functions
+
+doubleMe x = x + x
+
+doubleUs x y = x + x + y + y
+
+doubleSmallNumber x = if x > 100 then x else x * 2
+
+maxJenslöv = "Max Jenslöv"
+
+-- Lists
+
+lostNumbers = [4, 8, 15, 16, 23, 42]
+
+-- Putting lists together with the ++ operator
+
+listA = [1, 2, 3, 4]
+
+listB = [5, 6, 7, 8]
+
+listC = listA ++ listB -- [1,2,3,4,5,6,7,8]
+
+helloWorld = "hello" ++ " " ++ "world"
+
+-- ++ operator walks through the whole list on the left side of the "++"-operator
+-- Watch out for using this operator with very large lists
+-- ":"-operator puts something in the beginning of the list
+
+smallCat = " SMALL CAT"
+
+aSmallCat = 'A' : smallCat
+
+numbers = 1 : [2, 3, 4, 5] -- [1,2,3,4,5]
+
+-- The ":"-operator takes a single variable and a list instead of the "++"-operator which takes two lists
+
+-- Use the "!!"-operator to get an element out of the list by index. The indices start at 0
+character = "Max Jenslöv" !! 4 -- 'J'
+
+fourthNumber = [1, 2, 3, 4, 5, 6] !! 4 -- 5
+
+-- Lists can contain lists that contains lists
+
+lists = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
