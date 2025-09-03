c32e5eacb2f30634b3f11fcb60dd6bf5bb50765e
Max Jenslöv <max@jenslov.se>
2025-09-03 12:09:51 +0200

    Type basics

--- lec2a.hs
+++ lec2a.hs
@@ -10,3 +10,20 @@
 
 -- chr :: Int -> Char, decodes int to char
 -- ord :: Char -> Int, encodes a char to a numeric code
+-- use the :module or :m Data.Char to import the module where the commands are defined
+
+-- Functions with more than one argument
+
+-- Write the type signature above the corresponding function definition
+xor :: Bool -> Bool -> Bool
+xor p q = (p || q) && not (p && q)
+
+-- Type inference
+isL :: Char -> Bool
+isL c = c == 'l'
+
+-- Both arguments of "==" must have the same type
+-- Can write type signatures of the same type in one signature separated by commas
+firstName, lastName :: String
+firstName = "Max"
+lastName = "Jenslov"
