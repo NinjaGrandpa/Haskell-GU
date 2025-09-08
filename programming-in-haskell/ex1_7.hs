2d060f8a01d35bc7d6da708807f64b8a449211f3
Max Jenslöv <max@jenslov.se>
2025-09-08 17:09:23 +0200

    ex1_7: questions 3-5

--- programming-in-haskell/ex1_7.hs
+++ programming-in-haskell/ex1_7.hs
@@ -31,9 +31,59 @@ x + 0
 = {applying +}
 x
 -}
-
 -- 3.Define a function product that produces the product of a list of numbers, and show using your definition that product [2,3,4] = 24.
+{- Answer:
+myProduct [2,3,4]
+= {applying myProduct}
+2 * myProduct [3,4]
+= {applying myProduct}
+2 * (3 * myProduct [4])
+= {applying myProduct}
+2 * (3 * (4 * myProduct []))
+= {applying myProduct}
+2 * (3 * (4 * 1))
+= {applying inner *}
+2 * (3 * 4)
+= {applying inner *}
+2 * 12
+= {applying inner *}
+24
+-}
+{-# LANGUAGE BlockArguments #-}
+
+myProduct :: (Num a) => [a] -> a
+myProduct [] = 1
+myProduct (n : ns) = n * myProduct ns
 
 -- 4.How should the definition of the function qsort be modified so that it produces a reverse sorted version of a list?
+-- Answer:
+rsort [] = []
+rsort (x : xs) = rsort larger ++ [x] ++ rsort smaller
+  where
+    larger = [b | b <- xs, b > x]
+    smaller = [a | a <- xs, a <= x]
 
 -- 5.What would be the effect of replacing <= by < in the original definition of qsort? Hint: consider the example qsort [2,2,3,1,1].
+{- Answer:
+Every element that should have equal order as x doesn't get added to the "smaller" list and is therefore removed
+
+Is adding the "equal" list faster than using <=? All equal values doesn't need matching with x...
+-}
+
+numbers = [1, 1, 2, 3, 4, 5, 5, 5, 5, 5, 6, 5, 111, 111, 111, 556, 556, 556, 1114445]
+
+nsort [] = []
+nsort (x : xs) = nsort smaller ++ equal ++ [x] ++ nsort larger
+  where
+    smaller = [a | a <- xs, a < x]
+    larger = [b | b <- xs, b > x]
+    equal = [c | c <- xs, c == x]
+
+qsort [] = []
+qsort (x : xs) = qsort smaller ++ equal ++ [x] ++ qsort larger
+  where
+    smaller = [a | a <- xs, a < x]
+    larger = [b | b <- xs, b > x]
+    equal = [c | c <- xs, c == x]
+
+test = nsort numbers == qsort numbers
