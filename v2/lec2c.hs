1a227c5175e9db7c6bf0ee398710b8958f42b4cc
Max Jenslöv <max@jenslov.se>
2025-09-10 15:23:36 +0200

    created v2/lec2c and ch5

---
+++ v2/lec2c.hs
@@ -0,0 +1,16 @@
+{- Lecture 2C: more list functions
+    and accumulating parameters -}
+-- Recap:
+-- Polymorphic fumctions
+-- Strings are lists!
+-- Common type classes:
+-- -- Used for overloading
+-- -- Show, Eq, Ord, Num
+
+-- Today:
+-- More list functions
+-- -- With multiple arguments
+-- Extra and accumulating parameters
+-- Property-based testing with QuickCheck
+-- Building an executable
+-- Local definitions
