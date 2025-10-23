7e149d38bc9f4a2abb36b1af4b5d06e86679349b
Max Jenslöv <max@jenslov.se>
2025-10-23 21:58:32 +0200

    started ex6 in ExJan25

--- exam/ExJan25.hs
+++ exam/ExJan25.hs
@@ -33,8 +33,8 @@ chop n xs = case (splitAt n xs) of
     (ys, []) -> [ys]
     (ys, zs) -> ys : chop n zs 
 
-prop_chop :: Property
-prop_chop = chop 2 [1,2,3,4,5,6] === [[1,2],[3,4],[5,6]]
+prop_chopa :: Property
+prop_chopa = chop 2 [1,2,3,4,5,6] === [[1,2],[3,4],[5,6]]
           .&&. chop 3 [1,2,3,4] === [[1,2,3],[4]]
           .&&. chop 42 [1,2,3,4,5] === [[1,2,3,4,5]]
 
@@ -46,8 +46,8 @@ data FUPster = FUPster {
 
 data Card = Card {
     name       :: String,
-    speciality :: Speciality,
-    birthyear  :: Int
+    birthyear  :: Int,
+    speciality :: Speciality
 }
 
 data Speciality = Programming | Logic | Types | Security | FormalMethods
@@ -58,4 +58,80 @@ data Player = Player {
     fupsters :: Int
 }
 
-fups :: [FUPster]
+cards :: [Card]
+cards =
+  [ Card { name       = "Alonzo Church"
+         , speciality = Logic
+         , birthyear  = 1903
+         }
+  , Card { name       = "Alan Turing"
+         , speciality = FormalMethods
+         , birthyear  = 1912
+         }
+  , Card { name       = "John Backus"
+         , speciality = Programming
+         , birthyear  = 1924
+         }
+  , Card { name       = "Robin Milner"
+         , speciality = Types
+         , birthyear  = 1934
+         }
+  , Card { name       = "Phil Wadler"
+         , speciality = Types
+         , birthyear  = 1956
+         }
+  , Card { name       = "John Hughes"
+         , speciality = Programming
+         , birthyear  = 1958
+         }
+  , Card { name       = "Paul Hudak"
+         , speciality = Programming
+         , birthyear  = 1952
+         }
+  , Card { name       = "Simon Peyton Jones"
+         , speciality = Types
+         , birthyear  = 1958
+         }
+  ]
+
+-- ** 4.
+
+readCard :: IO Card
+readCard = do
+    putStr "Please give the legend name:\n> "
+    name <- getLine
+    putStrLn "Year of birth:\n> "
+    y <- readLn
+    putStrLn "Speciality:\n> "
+    s <- getLine
+    spec <- getSpeciality s
+    return (Card name y spec)
+
+getSpeciality :: String -> IO Speciality
+getSpeciality s = undefined
+
+-- ** 5.
+data List = Empty | Skip List | Cons Int List deriving Show
+
+xs, ys :: List
+xs = Cons 1 (Cons 2 (Skip (Cons 4 Empty)))
+ys = Cons 1 (Skip (Cons 3 (Skip (Cons 5 Empty))))
+
+-- | Converts a list of type List into a list of Maybe:s
+toList :: List -> [Maybe Int]
+toList xs = case xs of
+    Empty -> []
+    Skip l -> Nothing : toList l
+    Cons n l -> Just n : toList l
+    
+
+prop_toList :: Property
+prop_toList = toList xs === [Just 1, Just 2, Nothing, Just 4] .&&.
+              toList ys === [Just 1, Nothing, Just 3, Nothing, Just 5]
+
+-- ** 6.
+
+-- | Checks that chop returns sub-lists of size n
+prop_chop :: Int -> [Int] -> Property
+prop_chop n xs = 
+    collect (length xs)$
